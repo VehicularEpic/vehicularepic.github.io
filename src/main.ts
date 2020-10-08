12f8152eeecfc742992679a1a51a6dce00ce98c6
VitorMac10 <vitormac3@hotmail.com>
2020-10-08 10:43:37 -0300

    Reactive game module

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 
 import WebGame from '@/modules/web-game'
+const game = Vue.observable(WebGame);
 
 Vue.config.productionTip = false;
-Vue.prototype.$game = WebGame;
+
+Vue.mixin({
+    computed: {
+        $game: {
+            get: () => game
+        }
+    }
+});
 
 new Vue({ render: h => h(App) })
     .$mount('#app');
