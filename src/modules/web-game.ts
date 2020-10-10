5389b6cec4a60927bba35f65147b2f32fb027ace
VitorMac10 <vitormac3@hotmail.com>
2020-10-10 12:38:06 -0300

    ModelFactory class for reading wavefront models

--- src/modules/web-game.ts
+++ src/modules/web-game.ts
@@ -1,3 +1,4 @@
+import path from 'path'
 import { mat4, quat } from 'gl-matrix'
 
 var handle: number = 0;
@@ -6,7 +7,6 @@ var GL: WebGL2RenderingContext;
 const files: string[] = [
     'drags'
 ];
-const models: Map<string, Model> = new Map();
 
 async function initialize() {
     window.addEventListener('resize', () => {
@@ -19,7 +19,7 @@ async function initialize() {
 
     for (const name of files) {
         const data = await (await fetch(`models/${name}.json`)).json();
-        models.set(name, new Model(data));
+        const model = await ModelFactory.create(data['model']);
     }
 
     window.dispatchEvent(new Event('resize'));
@@ -165,35 +165,89 @@ class VertexArray {
 
 }
 
+class ModelFactory {
+
+    public static async create(object: string): Promise<Model> {
+        const vertices: number[][] = [];
+        const colors: number[][] = [];
+        const normals: number[][] = [];
+
+        const data = await (await fetch(`models/${object}`)).text();
+
+        function vec3(type: 'v' | 'vn'): number[][] {
+            const values: number[][] = [];
+            const regex = new RegExp(`${type}\\s(.+?)$`, 'gm');
+
+            for (let a; (a = regex.exec(data)) !== null;) {
+                values.push(a[1].split(/\s/).map(i => Number(i)));
+            }
+
+            return values;
+        }
+
+        const v = vec3('v'), vn = vec3('vn');
+        const materials = await (async () => {
+            const values: {
+                [material: string]: number[]
+            } = {};
+
+            const mtllib = /mtllib\s(.+?)$/gm.exec(data);
+            if (mtllib !== null) {
+                const file = path.basename(mtllib[1]);
+                const rgx1 = /newmtl\s(.+?)$/gm, rgx2 = /Kd\s(.+?)$/gm;
+                const mtldata = await (await fetch(`models/${file}`)).text();
+
+                for (let a; (a = rgx1.exec(mtldata)) !== null;) {
+                    values[a[1]] = (rgx2.exec(mtldata) as RegExpExecArray)[1]
+                        .split(/\s/).map(i => Number(i));
+                }
+
+                return values;
+            }
+
+            values['default'] = [0.25, 0.25, 0.25];
+            return values;
+        })();
+
+        var material: string = 'default';
+        data.split(/\n/gm).filter(s =>
+            /^(usemtl|f)/.test(s)
+        ).map(e => e.split(/\s/, 4)).forEach(e => {
+            if (e[0] === 'usemtl') {
+                material = e[1];
+            } else if (e[0] === 'f') {
+                e.slice(1).forEach(i => {
+                    const indices = i.split(/\/\//g)
+                        .map(j => Number(j));
+
+                    vertices.push(v[indices[0] - 1]);
+                    colors.push(materials[material]);
+                    normals.push(vn[indices[1] - 1]);
+                });
+            }
+        });
+
+        return new Model({ vertices, colors, normals });
+    }
+
+}
+
 class Model {
 
     private readonly length: number;
     private readonly buffer: VertexArray;
 
-    constructor(object: {
-        materials: {
-            [name: string]: number[]
-        },
-        normals: number[][],
+    constructor(data: {
         vertices: number[][],
-        faces: {
-            [name: string]: number[][]
-        }
+        colors: number[][],
+        normals: number[][],
     }) {
         this.buffer = new VertexArray();
-        const vertices: number[][] = [], colors: number[][] = [];
+        this.length = data.vertices.length;
 
-        Object.keys(object.faces).forEach(e => {
-            const material = object.materials[e];
-            object.faces[e].flat().forEach(a => {
-                vertices.push(object.vertices[a - 1]);
-                colors.push(material);
-            });
-        });
-
-        this.length = vertices.length;
-        this.buffer.push(vertices.flat());
-        this.buffer.push(colors.flat());
+        this.buffer.push(data.vertices.flat());
+        this.buffer.push(data.colors.flat());
+        this.buffer.push(data.normals.flat());
     }
 
     public render(): void {
