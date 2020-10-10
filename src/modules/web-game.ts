bb860919584e0af54ae46e642bfcbb8b568f827c
VitorMac10 <vitormac3@hotmail.com>
2020-10-09 23:23:08 -0300

    Matrix calculation utils

--- src/modules/web-game.ts
+++ src/modules/web-game.ts
@@ -1,3 +1,5 @@
+import { mat4, quat } from 'gl-matrix'
+
 var handle: number = 0;
 var GL: WebGL2RenderingContext;
 
@@ -43,6 +45,46 @@ export class WebGame {
 
     public set context(context: WebGL2RenderingContext) {
         GL = context;
+    }
+
+}
+
+class MatrixUtils {
+
+    public static perspective(fovY: number, aspect: number): mat4 {
+        const near = 0.01;
+        const height = near * Math.tan((fovY / 2) * (Math.PI / 180.0));
+
+        const matrix = mat4.create();
+        mat4.frustum(matrix, -(height * aspect), height * aspect, -height, height, near, 0);
+
+        const e = 1E-6;
+        matrix[10] = e - 1.0;
+        matrix[14] = (e - 2.0) * near;
+        return matrix;
+    }
+
+    public static lookAt(x: number, y: number, z: number, cx: number, cy: number, cz: number): mat4 {
+        return mat4.lookAt(
+            mat4.create(),
+            [x, y, z],
+            [cx, cy, cz],
+            [0.0, -1.0, 0.0]
+        );
+    }
+
+    public static transformation(x: number, y: number, z: number, xz: number, zy: number, xy: number): mat4 {
+        const quat_xz = quat.setAxisAngle(quat.create(), [0.0, -1.0, 0.0], xz);
+        const quat_zy = quat.setAxisAngle(quat.create(), [1.0, 0.0, 0.0], zy);
+        const quat_xy = quat.setAxisAngle(quat.create(), [0.0, 0.0, 1.0], xy);
+
+        const rotated = quat.create();
+        quat.mul(rotated, quat_xz, quat_zy);
+        quat.mul(rotated, rotated, quat_xy);
+
+        return mat4.fromRotationTranslationScale(mat4.create(),
+            rotated, [x, y, z], [1.0, 1.0, 1.0]
+        );
     }
 
 }
